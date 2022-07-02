fb09e26752447992b30cbb568e263472fcf0d444
Elliot-IA <alexa818@umn.edu>
2022-07-02 13:13:49 -0500

    autoCommit

--- js/main.js
+++ js/main.js
@@ -678,13 +678,14 @@ function deleteEntry(){
 
     console.log("modMessage: "+modMessage);
 
-    if(device == "webpage"){
+    $.post("/", {command: "modData", data: modMessage});
+    /*if(device == "webpage"){
         document.getElementById("command_hiddenInput").value = "modData";
         document.getElementById("data_hiddenInput").value = modMessage;
         document.getElementById("hiddenForm").submit();
     }else{
         $.post("/", {data: modMessage});
-    }
+    }*/
 
     Inventory.splice(showingResultIndex, 1);
     //document.getElementById("tilesHolder").children[eval(showingResultIndex)+1].remove();
