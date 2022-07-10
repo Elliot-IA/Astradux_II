e48f5aa1ab564e2dbbf73e5546c3b51d04d17a55
Elliot-IA <alexa818@umn.edu>
2022-07-10 15:14:15 -0500

    autoCommit

--- js/addPart.js
+++ js/addPart.js
@@ -562,12 +562,11 @@ var inputElement = document.getElementById('picInput');
 
 imageElement.addEventListener("drop", (e)=>{
     imageElement.src = "Images/subtleLoading.gif";
-    var droppedFiles = e.dataTransfer.files;
+    var droppedFiles = e.dataTransfer.files
 
-    console.log("Drop Heard! Here's what I captured: "+droppedFiles);
+    console.log("Drop Heard!");
     //Future: droppedFiles can hold multiple files! Good jumping off point for assigning multiple images to the same part
     if(droppedFiles.length == 1){
-
 
         var imgName = droppedFiles[0].name;
         console.log("The file you dropped is called\""+imgName+"\"");
@@ -575,8 +574,8 @@ imageElement.addEventListener("drop", (e)=>{
         //picURL = imgName;
         picURL = createTimestamp();
 
-        var droppedFiles = e.dataTransfer.files;
-        console.log("Drop Heard! Here's what I captured:");
+        //var droppedFiles = e.dataTransfer.files;
+        console.log("Here's what I captured:");
         console.log(droppedFiles);
 
         convertToURI(droppedFiles[0]);
@@ -625,6 +624,8 @@ inputElement.addEventListener("change",(e)=>{
     storeURIData();
 });
 
+
+
 function storeURIData(){
     console.log("Stroing image uri data and timestamp...");
     timestamp_picName = createTimestamp();
@@ -646,6 +647,7 @@ function convertToURI(imgToConvert){
     reader.addEventListener("load", ()=>{
         picURI = reader.result;
         console.log("~URI TRANSLATION COMPLETE!...");
+        resizeImgWrapper(picURI);
         if(!freshImg){      //This tells us if img is for a location or not
             imageElement.src = picURI;           
         }else{
@@ -953,6 +955,7 @@ function cam_caputeImg(){
     setTimeout(()=>{
         $("#name")[0].focus();
     },0);
+    resizeImgWrapper(photo);
 }
 
 function createTimestamp(){
@@ -970,7 +973,7 @@ function createTimestamp(){
 
 
 
-
+/*-------------------------IMG RESIZING STUFF-------------------------*/
 $(document).ready(function() {
 
     $('#imageFile').change(function(evt) {
@@ -988,52 +991,94 @@ $(document).ready(function() {
     });
 });
 
-function ResizeImage() {
-    if (window.File && window.FileReader && window.FileList && window.Blob) {
-        var filesToUploads = document.getElementById('imageFile').files;
-        var file = filesToUploads[0];
-        if (file) {
+function dataURItoBlob(dataURI) {
+    // convert base64/URLEncoded data component to raw binary data held in a string
+    var byteString;
+    if (dataURI.split(',')[0].indexOf('base64') >= 0)
+        byteString = atob(dataURI.split(',')[1]);
+    else
+        byteString = unescape(dataURI.split(',')[1]);
 
+    // separate out the mime component
+    var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+
+    // write the bytes of the string to a typed array
+    var ia = new Uint8Array(byteString.length);
+    for (var i = 0; i < byteString.length; i++) {
+        ia[i] = byteString.charCodeAt(i);
+    }
+
+    return new Blob([ia], {type:mimeString});
+}
+function ResizeImage(uri) {
+
+    //------------Convert the URI back to a file Object------------//
+    var fileType = uri.substring(uri.indexOf(":")+1, uri.indexOf(";"))
+
+    var myBlob = dataURItoBlob(uri);
+
+    var file = new File([myBlob], fileType.replaceAll("/",".") /*'image.jpeg'*/, {
+        type: myBlob.type,
+    });
+
+
+    //------------------------------------------------------------//
+
+
+    if (window.File && window.FileReader && window.FileList && window.Blob) {
+        /*var filesToUploads = document.getElementById('imageFile').files;
+        var file = filesToUploads[0];*/
+        if (file) {
             var reader = new FileReader();
             // Set the image once loaded into file reader
             reader.onload = function(e) {
-
                 var img = document.createElement("img");
                 img.src = e.target.result;
-
                 var canvas = document.createElement("canvas");
+                //document.body.appendChild(canvas);
                 var ctx = canvas.getContext("2d");
                 ctx.drawImage(img, 0, 0);
-
-                var MAX_WIDTH = 200;
-                var MAX_HEIGHT = 200;
+                var MAX_WIDTH = 2500;
+                var MAX_HEIGHT = 2500;
                 var width = img.width;
                 var height = img.height;
-
+                var resizeNeeded = false;
                 if (width > height) {
                     if (width > MAX_WIDTH) {
                         height *= MAX_WIDTH / width;
                         width = MAX_WIDTH;
+                        resizeNeeded = true;
                     }
                 } else {
                     if (height > MAX_HEIGHT) {
                         width *= MAX_HEIGHT / height;
                         height = MAX_HEIGHT;
+                        resizeNeeded = true;
                     }
                 }
                 canvas.width = width;
                 canvas.height = height;
-                var ctx = canvas.getContext("2d");
-                ctx.drawImage(img, 0, 0, width, height);
-
-                dataurl = canvas.toDataURL(file.type);
-                document.getElementById('output').src = dataurl;
+                if(resizeNeeded){
+                    console.log("Image found to be too big! Scaling down and resaving URI...");
+                    //canvas.style = "background-color:blue";
+                    var ctx = canvas.getContext("2d");
+                    ctx.drawImage(img, 0, 0, width, height);
+                    var smallerURI = canvas.toDataURL(file.type);
+                    //document.getElementById('output').src = dataurl;
+                    jQuery($("#uri_hiddenInput")[0]).attr("value",smallerURI);
+                    console.log("~Re-sized URI storage successful~");
+                }else{
+                    console.log("Image found to be within acceptable size bounds");
+                }
             }
             reader.readAsDataURL(file);
-
         }
 
     } else {
         alert('The File APIs are not fully supported in this browser.');
     }
+}
+function resizeImgWrapper(uri){
+    console.log("Determining if image is too large to be POST-ed...");
+    ResizeImage(uri);
 }
