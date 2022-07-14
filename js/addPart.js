04cad9b329686f2a7281b9d4e7f962bad47175f8
Elliot-IA <alexa818@umn.edu>
2022-07-14 07:30:23 -0500

    autoCommit

--- js/addPart.js
+++ js/addPart.js
@@ -905,8 +905,7 @@ function imageInputClick(){
     if(device == "webpage"){
         document.getElementById('picInput').click();
     }else{
-        inputElement.accept = "";
-        inputElement.capture = "";
+        document.getElementById('picInput').outerHTML = "<input type=\"file\" id=\"picInput\" accept=\"application/jpg, application/png, application/svg, application/gif, application/jpeg\" style=\"display: none;\" onchange=\"refreshImage()\">";
         document.getElementById('picInput').click();
     }
 }
